5ce6c5e88bb74d9e1897cc2b7200d94fdf395ca6
Alwin Kuruvilla <alwin@akxlphotos.com>
2021-03-01 23:06:25 -0500

    Completed calculation function. Working on clear function.

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,3 +1,8 @@
+let number1 = 0;
+let number2 = 0;
+let operations;
+let selectedOperation;
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -14,43 +19,42 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-function checkForZero(num) {
-    if(num === 0) {
-        return true;
-    } else return false;
-}
-
 function getInput() {
-    let number1 = document.getElementById("first-number").value;
-    let number2 = document.getElementById("second-number").value;
-    let operation = document.getElementsByName("operation").checked.value;
+    number1 = document.getElementById("first-number-box").value;
+    number2 = document.getElementById("second-number-box").value;
+    operations = document.getElementsByName("operation");
+    for(let i=0; i<operations.length; i++) {
+        if (operations[i].checked) {
+            selectedOperation = operations[i].value;
+        }
+    }
 }
 
 function calculate() {
-    getInput();
-    switch(opeation) {
+    let num1 = parseInt(number1);
+    let num2 = parseInt(number2);
+    switch(selectedOperation) {
         case 'add':
-            return add(number1, number2);
+            return add(num1, num2);
         case 'subtract':
-            return subtract(number1, number2);
+            return subtract(num1, num2);
         case 'multiply':
-            return multiply(number1, number2);
+            return multiply(num1, num2);
         case 'divide':
-            if (number2 === 0") {
-            return 'Dividing by 0 is not allowed'
+            if (num2 === 0) {
+                return 'Dividing by 0 is not allowed'
+            } return divide(num1, num2);
     }
-    return divide(number1, number2);
-}
 }
 
-// <p id="demo">Click the button to replace this document with new content.</p>
+function run() {
+    getInput();
+    console.log(calculate());
+    document.getElementById('answer').innerText = calculate() ;
+}
 
-// <button onclick="myFunction()">Try it</button>
-
-// <script>
-// function myFunction() {
-//   document.open("text/html","replace");
-//   document.write("<h2>Learning about the HTML DOM is fun!</h2>");
-//   document.close();
-// }
-// </script>
+function clear() {
+    number1 = "";
+    number2 = "";
+    selectedOperation.checked = undefined;
+}
